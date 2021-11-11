b3a34f58112982a1bdadef355d2c4eaeec00e239
lself <lself@manifestcorp.com>
2021-11-11 14:46:39 -0500

    Lab 2B solution

--- src/app/components/book-component/book.component.spec.ts
+++ src/app/components/book-component/book.component.spec.ts
@@ -1,8 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookComponent } from './book.component';
+import {By} from "@angular/platform-browser";
+import {DebugElement} from "@angular/core";
 
-describe('BookComponentComponent', () => {
+describe('BookComponent', () => {
   let component: BookComponent;
   let fixture: ComponentFixture<BookComponent>;
 
@@ -21,5 +23,14 @@ describe('BookComponentComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('component', () => {
+    it('has a label for the title with the value of "Title:" and displays the book title', () => {
+      let titleElement: DebugElement = fixture.debugElement.query(By.css('#title'));
+      expect(titleElement).toBeTruthy();
+      expect(titleElement.nativeElement.textContent).toContain('Title:');
+      expect(titleElement.nativeElement.textContent).toContain(component.book.title);
+    });
   });
 });
